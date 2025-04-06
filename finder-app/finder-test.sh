0a2070575b5ae73349167c40e89116e59f1a5fda
Kevin Diaz <kevrocks67@gmail.com>
2025-04-06 17:32:08 -0400

    Update code for assignment 4 part 2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,15 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+
+if [ -d /etc/finder-app/conf ];then
+    CONF_PATH="/etc/finder-app/conf"
+else
+    CONF_PATH="conf"
+    PATH=$(pwd):$PATH
+fi
+
+username=$(cat ${CONF_PATH}/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +40,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat ${CONF_PATH}/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -54,16 +62,17 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-    ./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+    writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
+echo ${OUTPUTSTRING} > /tmp/assignment4-result.txt
 if [ $? -eq 0 ]; then
 	echo "success"
 	exit 0
